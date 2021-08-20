9ec06aa036af669de1b54896ba3682b195dce07a
CarlosInesMillan <a01740794@itesm.mx>
2021-08-20 20:31:50 +0000

    Ejercicios sesion 4

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,6 +1,20 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    num = int(input("Ingresa tu edad: "))
+    if 0<num<18:
+        print("No cumples requisitos")
+    elif num<=0:
+        print("Respuesta incorrecta")
+    else:
+        lic = input("¿Tienes identificación oficial? (s/n): ")
+        if lic == "s":
+            print("Trámite de licencia concedido")
+        elif lic =="n":
+            print("No cumples requisitos")
+        else:
+            print("Respuesta incorrecta")
+            
+
 
 if __name__ == '__main__':
     main()
